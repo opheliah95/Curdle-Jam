c5fff40cba1318fd806240457692df137c89d066
Russell Brabers <brabers.russell@gmail.com>
2020-04-25 15:00:38 -0400

    Revert "Prefab update"
    This reverts commit 37b256f646c3cefb2f2ec6cbcbbd69b0e2c8277c.

--- Assets/Scripts/PlayerControllerForce.cs
+++ Assets/Scripts/PlayerControllerForce.cs
@@ -29,7 +29,6 @@ public class PlayerControllerForce : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
