37b256f646c3cefb2f2ec6cbcbbd69b0e2c8277c
Russell Brabers <brabers.russell@gmail.com>
2020-04-25 14:55:19 -0400

    Prefab update
    - Created block prefabs
    - Created spawner script
    - Added block script to prefabs

---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float fallSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        fallSpeed = 100.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0, -fallSpeed * Time.deltaTime, 0);
+    }
+}
